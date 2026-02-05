0ed30d8e559917dfdd0683f50f3ca2711501813f
Marcus Fenner <git@mfen.de>
2026-02-05 20:45:22 +0100

    structural integrity: legal foundation and search protocols

--- astro.config.ts
+++ astro.config.ts
@@ -11,12 +11,36 @@ export default defineConfig({
       filter: (page) =>
         page !== "https://taegli.ch/" &&
         page !== "https://taegli.ch/today/" &&
-        page !== "https://taegli.ch/404/",
+        page !== "https://taegli.ch/404/" &&
+        page !== "https://taegli.ch/imprint/" &&
+        page !== "https://taegli.ch/privacy/" &&
+        page !== "https://taegli.ch/acknowledgements/",
     }),
   ],
   env: {
     schema: {
       TAEGLICH_SALT: envField.string({ context: "server", access: "secret" }),
+      PUBLIC_IMPRINT_NAME: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_IMPRINT_ADDRESS_LINE1: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_IMPRINT_ADDRESS_LINE2: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      PUBLIC_IMPRINT_ADDRESS_LINE3: envField.string({
+        context: "client",
+        access: "public",
+        optional: true,
+      }),
+      PUBLIC_IMPRINT_EMAIL: envField.string({
+        context: "client",
+        access: "public",
+      }),
     },
   },
   vite: {
