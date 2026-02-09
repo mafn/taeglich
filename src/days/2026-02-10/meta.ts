7fa7ad758f5f18a4566745fd28a14bbe22c9c272
Marcus Fenner <git@mfen.de>
2026-02-09 23:25:12 +0100

    i decided to go running (#17)

---
+++ src/days/2026-02-10/meta.ts
@@ -0,0 +1,9 @@
+import { defineDay } from "../../lib/day";
+
+export default defineDay({
+  title: "I Decided To Go Running",
+  description:
+    "A one-button run where every outcome is a loss: hazards, collapse, or the bed at the finish line.",
+  tags: ["game", "runner", "jump", "exhaustion", "night"],
+  shellMode: "immersive",
+});
