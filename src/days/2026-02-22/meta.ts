9af1b82a1022b644d412f893f0315ebc072acf49
Marcus Fenner <git@mfen.de>
2026-07-13 19:08:01 +0200

    The Doppelkopf pause finally takes its seat (#21)

---
+++ src/days/2026-02-22/meta.ts
@@ -0,0 +1,9 @@
+import { defineDay } from "../../lib/day";
+
+export default defineDay({
+  title: "Sorry",
+  description: "Decided to play Doppelkopf for a few days.",
+  tags: ["note", "doppelkopf"],
+  shellMode: "compact",
+  renderH1: false,
+});
